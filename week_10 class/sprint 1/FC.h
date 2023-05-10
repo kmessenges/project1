cfc80c343084ce3ddbc4fad9333eb2d0e3bdd5de
kmessenges <127204836+kmessenges@users.noreply.github.com>
2023-05-10 16:48:10 +0900

    Delete week_10 class/sprint 1 directory

--- week_10 class/sprint 1/FC.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class FC {
-public:
-	FC() {};
-	FC(float cc) { c = cc; f = getF(); };
-	float c, f;
-	float getF() { return (c * 9. / 5. + 32.0); }
-	float getC() { return ((f - 32.0) * 5. / 9.); }
-};
