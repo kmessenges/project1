9e917f42f65b4e05c3e828978b0785d5fc497d80
kmessenges <127204836+kmessenges@users.noreply.github.com>
2023-06-01 15:36:12 +0900

    Create SuvS3.h

---
+++ week13_practice/SuvS3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CarS3.h"
+class SUV : public Car {
+private:
+	int spareWheels;
+public:
+	void klaxon(int);
+	void setSparewheels(int);
+	int getSparewheels();
+	float getTotalPrice();
+	int callmynum();
+};
